7c03859c5df781faf8cbf816a8ff3db25507d0cf
Josh Triplett <josh@joshtriplett.org>
2011-05-03 13:45:05 -0700

    Add an initial prototype of the Apters client
    Supports creating a workspace and cloning a store tag.  Currently
    accesses the store via git.
    
    Commit by Josh Triplett and Jamey Sharp.

---
+++ Client.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import System.Directory
+import System.FilePath
+import System.Environment
+import System.Exit
+import System.Process
+
+clone :: [String] -> IO ()
+clone [tag] = clone [tag, tag]
+clone [tag, name] = do
+    store <- readFile (".apters" </> "store")
+    ExitSuccess <- rawSystem "git" ["init", name]
+    (Nothing, Nothing, Nothing, h) <- createProcess (proc "git" ["pull", store, "tag", tag]) { cwd = Just name }
+    ExitSuccess <- waitForProcess h
+    return ()
+clone _ = putStrLn "Usage: apters clone <store tag> [<directory>]"
+
+workspace :: [String] -> IO ()
+workspace [url, dir] = do
+    createDirectory dir
+    let aptersDir = dir </> ".apters"
+    createDirectory aptersDir
+    writeFile (aptersDir </> "store") url
+workspace _ = putStrLn "Usage: apters workspace <store url> <directory>"
+
+help :: [String] -> IO ()
+help [] = do
+    putStrLn "Usage: apters <command> [<args>]\n\nApters commands:"
+    mapM_ (putStrLn . fst) cmds
+help (cmd:_) = case lookup cmd cmds of
+    Just _ -> putStrLn $ "apters: no help on " ++ cmd ++ " for you!"
+    Nothing -> putStrLn $ "apters: '" ++ cmd ++ "' is not an apters command. See 'apters help'."
+
+cmds :: [(String, [String] -> IO ())]
+cmds = [("clone", clone),
+        ("workspace", workspace),
+        ("help", help)]
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [] -> help []
+        cmd:cmdargs -> case lookup cmd cmds of
+            Just f -> f cmdargs
+            Nothing -> help args
