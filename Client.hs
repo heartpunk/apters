d26c399dfe5eb0ea3e4305e303abd06b2263e457
Josh Triplett <josh@joshtriplett.org>
2011-05-03 15:53:28 -0700

    Add a command to expand a dep.
    Commit by Josh Triplett and Jamey Sharp.

--- Client.hs
+++ Client.hs
@@ -1,11 +1,16 @@
 module Main where
 
+import Control.Exception
+import Control.Monad
+import Data.List
 import System.Directory
 import System.FilePath
 import System.Environment
 import System.Exit
+import System.IO.Error
 import System.Process
 
+import DepsScanner
 import Store
 
 clone :: [String] -> IO ()
@@ -18,6 +23,16 @@ clone [tag, name] = do
     ExitSuccess <- waitForProcess h
     return ()
 clone _ = putStrLn "Usage: apters clone <store tag> [<directory>]"
+
+expand :: [String] -> IO ()
+expand [parent, depname, child] = do
+    depsStr <- readFile $ parent </> "apters.deps"
+    let Just dep = lookup depname (getDeps depsStr)
+    clone [dep, child]
+    links <- readLinks
+    writeLinks $ (parent, depname, child) : [l | l@(parent', depname', _) <- links, parent /= parent', depname /= depname']
+    return ()
+expand _ = putStrLn "Usage: apters expand <parent_dir> <dependency> <child_dir>"
 
 workspace :: [String] -> IO ()
 workspace [url, dir] = do
@@ -37,8 +52,30 @@ help (cmd:_) = case lookup cmd cmds of
 
 cmds :: [(String, [String] -> IO ())]
 cmds = [("clone", clone),
+        ("expand", expand),
         ("workspace", workspace),
         ("help", help)]
+
+readLinks :: IO [(String, String, String)]
+readLinks = do
+    handleJust (guard . isDoesNotExistError) (const $ return []) $ liftM (take3s . split '\0') $ readFile $ ".apters" </> "links"
+    where
+    take3s :: [a] -> [(a, a, a)]
+    take3s [] = []
+    take3s (x:y:z:xs) = (x, y, z) : take3s xs
+    take3s _ = error "links file corrupt"
+    split :: Eq a => a -> [a] -> [[a]]
+    split _ [] = []
+    split x xs = case break (== x) xs of (v, xs') -> v : split x (drop 1 xs')
+
+writeLinks :: [(String, String, String)] -> IO ()
+writeLinks links = do
+    writeFile (".apters" </> "links.tmp") $ intercalate "\0" $ join3s links
+    renameFile (".apters" </> "links.tmp") (".apters" </> "links")
+    where
+    join3s :: [(a, a, a)] -> [a]
+    join3s [] = []
+    join3s ((x, y, z) : xs) = x:y:z:join3s xs
 
 main :: IO ()
 main = do
